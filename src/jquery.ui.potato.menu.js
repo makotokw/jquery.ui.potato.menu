004ec1b73b95a050f030dd0b8e848a7efedb6d80
Daniel Kolakowski <dan.zissou@gmail.com>
2012-01-03 19:41:54 +0100

    Added alignBottom option to enable different positioning strategy for vertical menus
    Added alignBottom option to enable different positioning strategy for
    vertical menus.
    Fixed grammar error in settings and class names.

--- src/jquery.ui.potato.menu.js
+++ src/jquery.ui.potato.menu.js
@@ -7,58 +7,62 @@
  * @author makoto_kw
  * @version 1.1
  */
-(function($) {
-	var defaults = {
-		vertical:false,
-		menuItemSelector: 'li',
-		menuGroupSelector: 'ul',
-		rootClass:'potato-menu',
-		menuItemClass:'potato-menu-item',
-		menuGroupClass:'potato-menu-group',
-		verticalClass:'potato-menu-vertical',
-		holizontalClass:'potato-menu-holizontal',
-		hasVerticalClass:'potato-menu-has-vertical',
-		hasHolizontalClass:'potato-menu-has-holizontal',
-		hoverClass:'potato-menu-hover',
-		showDuration: 350,
-		hideDuration: 100
-	}
-	function menu() {
-		var option = (typeof(arguments[0])!='string') ? $.extend(defaults,arguments[0]) : $.extend(defaults,{});
-		var $menu = $(this).addClass(option.rootClass+' '+option.menuGroupClass).addClass((option.vertical) ? option.verticalClass : option.holizontalClass);
-		var $menuItems = $menu.find(option.menuItemSelector).addClass(option.menuItemClass);
-		var $menuGroups = $menu.find(option.menuGroupSelector).addClass(option.menuGroupClass);
-		
-		$menuItems.hover(
-			function(e) {
-				$(this).addClass(option.hoverClass);
+(function ($) {
+    var defaults = {
+        vertical: false,
+        alignBottom: false,
+        menuItemSelector: 'li',
+        menuGroupSelector: 'ul',
+        rootClass: 'potato-menu',
+        menuItemClass: 'potato-menu-item',
+        menuGroupClass: 'potato-menu-group',
+        verticalClass: 'potato-menu-vertical',
+        horizontalClass: 'potato-menu-horizontal',
+        hasVerticalClass: 'potato-menu-has-vertical',
+        hashorizontalClass: 'potato-menu-has-horizontal',
+        hoverClass: 'potato-menu-hover',
+        showDuration: 350,
+        hideDuration: 100
+    }
+    function menu() {
+        var option = (typeof (arguments[0]) != 'string') ? $.extend(defaults, arguments[0]) : $.extend(defaults, {});
+        var verticalAlignBottom = option.alignBottom === true;
+        var $menu = $(this).addClass(option.rootClass + ' ' + option.menuGroupClass).addClass((option.vertical) ? option.verticalClass : option.horizontalClass);
+        var $menuItems = $menu.find(option.menuItemSelector).addClass(option.menuItemClass);
+        var $menuGroups = $menu.find(option.menuGroupSelector).addClass(option.menuGroupClass);
+
+        $menuItems.hover(
+			function (e) {
+			    $(this).addClass(option.hoverClass);
 			},
-			function(e) {
-				$(this).removeClass(option.hoverClass);
+			function (e) {
+			    $(this).removeClass(option.hoverClass);
 			}
 		);
-		$menuGroups.parent().each(function(index){
-			var $parentMenuItem = $(this); // menu item that has menu group
-			var displayDirection = ($parentMenuItem.parent().hasClass(option.holizontalClass)) ? 'bottom' : 'right';
-			$parentMenuItem.addClass((displayDirection=='bottom') ? option.hasVerticalClass : option.hasHolizontalClass);
-			var $menuGroup = $parentMenuItem.find(option.menuGroupSelector+':first').addClass(option.verticalClass);
-			$parentMenuItem.hover(
-				function(e) {
-					var offset = (displayDirection=='bottom') ? {left:'0',top:''} : {left:$(this).width()+'px',top:'0'};
-					$menuGroup.css({left:offset.left,top:offset.top}).fadeIn(option.showDuration);
+        $menuGroups.parent().each(function (index) {
+            var $parentMenuItem = $(this); // menu item that has menu group
+            var displayDirection = ($parentMenuItem.parent().hasClass(option.horizontalClass)) ? 'bottom' : 'right';
+            $parentMenuItem.addClass((displayDirection == 'bottom') ? option.hasVerticalClass : option.hashorizontalClass);
+            var $menuGroup = $parentMenuItem.find(option.menuGroupSelector + ':first').addClass(option.verticalClass);
+            $parentMenuItem.hover(
+				function (e) {
+				    var offset = (displayDirection == 'bottom') ?
+                        { left: '0', top: ''} :
+                        { left: $(this).width() + 'px', top: verticalAlignBottom ? ($parentMenuItem.height() - $menuGroup.height()) + 'px' : '0px' };
+				    $menuGroup.css({ left: offset.left, top: offset.top }).fadeIn(option.showDuration);
 				},
-				function(e) {
-					$menuGroup.fadeOut(option.hideDuration);
+				function (e) {
+				    $menuGroup.fadeOut(option.hideDuration);
 				}
 			);
-		});
-		$menu.find('a[href^="#"]').click(function() {
-			$menuGroups.fadeOut(option.hideDuration);
-			return ($(this).attr('href') != '#');
-		})
-		return this;
-	}
-	$.fn.extend({
-		ptMenu:menu
-	});
+        });
+        $menu.find('a[href^="#"]').click(function () {
+            $menuGroups.fadeOut(option.hideDuration);
+            return ($(this).attr('href') != '#');
+        })
+        return this;
+    }
+    $.fn.extend({
+        ptMenu: menu
+    });
 })(jQuery);
